4d94df62899a6b0fdf56776051e205e7881b3bd4
Daisy Molving <daisymolving@gmail.com>
2016-02-17 15:41:08 +0000

    wrote method that finds a word from the dictionary within the input and returns its frequency in a hash. had to rewrite 1st test as we should expect the result to be an empty hash and not nil

--- substring.rb
+++ substring.rb
@@ -1,7 +1,13 @@
 class Substring 
 
   def find_substring(input, dictionary)
-	 new_hash = {"is" => 1}
-	 new_hash
+	
+	 substring_frequency = Hash.new(0)
+	 dictionary.each do |word|
+		if input.include? word
+		  substring_frequency[word] += 1
+		end
+	 end
+	 substring_frequency
   end
 end
--- substring_spec.rb
+++ substring_spec.rb
@@ -1,10 +1,10 @@
 require_relative './substring'
 describe Substring do
-  it 'should return nil if given the input "cat"' do
+  it 'should return an empty hash if given the input "cat"' do
 	 substring = Substring.new
 	 dictionary = %w(hello how are you going down there here is the low word or his story)
 	 result = substring.find_substring("cat", dictionary)
-	 expect(result).to eq nil
+	 expect(result).to eq ({})
   end
 
   it 'should return {"is" => 1} if the input is "is"' do
