100ac3647b684e31d740bc355e66f57969a5db01
Daisy Molving <daisymolving@gmail.com>
2016-02-17 15:53:42 +0000

    wrote a test to ensure that when input is multiple words the method still works to find substringfrequencies of all possible dictionary words

--- substring_spec.rb
+++ substring_spec.rb
@@ -21,5 +21,11 @@ describe Substring do
 	 expect(result).to eq ({"is"=>1, "his"=>1})
   end
 
+  it 'should return {"is" => 1, "his" => 1, "there" => 1, "here" => 1, "the" => 1} if the input is "his there"' do
+	 substring = Substring.new
+	 dictionary = %w(hello how are you going down there here is the low word or his story)
+	 result = substring.find_substring("his there", dictionary)
+	 expect(result).to eq ({"is"=>1, "his"=>1, "there"=>1, "the" => 1,  "here"=>1})
+  end
 end
 
